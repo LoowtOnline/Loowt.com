7da69845998e482f586e1120bc6c0530e05a2b1e
LoowtOnline <129815932+LoowtOnline@users.noreply.github.com>
2023-04-12 16:32:36 -0300

    Loowt.Online

---
+++ Loowt - Acervo/Loowt - Acervo 1/Loowt Java.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+}); 
+
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
+
+
